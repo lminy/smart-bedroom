0ac752a1a5529054544d128470cd4e3edeb28ea1
lminy <laurent.miny@hotmail.com>
2016-03-24 23:04:10 +0100

    Optimisation PlaylistController + récupération des chemins des playlists depuis le fichier pplication.conf

--- app/controllers/PlaylistsController.scala
+++ app/controllers/PlaylistsController.scala
@@ -5,6 +5,7 @@ import play.api._
 import play.api.mvc._
 
 import java.io.File
+import scala.collection.JavaConversions._
 
 /**
 * This controller creates an `Action` to handle HTTP requests to the
@@ -12,10 +13,23 @@ import java.io.File
 */
 @Singleton
 class PlaylistsController @Inject() extends Controller {
-
+/*
     val playlists = Map("good-mood" -> "E:/Projects/Mobile/good-mood",
                         "cool-off"  -> "E:/Projects/Mobile/cool-off",
                         "alarm-clock" -> "E:/Projects/Mobile/alarm-clock")
+*/
+    def playlists: Map[String, String] = (for{
+        configuration <- play.Play.application.configuration.getConfigList("playlists")
+    } yield (configuration.getString("name"), configuration.getString("path"))).toMap
+
+    def getListOfFiles(dir: String):List[File] = {
+        val d = new File(dir)
+        if (d.exists && d.isDirectory) {
+            d.listFiles.filter(_.isFile).toList
+        } else {
+            List[File]()
+        }
+    }
 
     /**
     * Create an Action to render an HTML page with a welcome message.
@@ -24,15 +38,6 @@ class PlaylistsController @Inject() extends Controller {
     * a path of `/`.
     */
     def index = Action {
-        def getListOfFiles(dir: String):List[File] = {
-            val d = new File(dir)
-            if (d.exists && d.isDirectory) {
-                d.listFiles.filter(_.isFile).toList
-            } else {
-                List[File]()
-            }
-        }
-
         val songs: Map[String,List[String]] = playlists.map{
             case (playlist, path) => (playlist -> (getListOfFiles(path).filter(_.getName().endsWith(".mp3"))).map(_.getName()))
         }
@@ -50,7 +55,7 @@ class PlaylistsController @Inject() extends Controller {
         request.body.file("song").map { song =>
             def moveSong(): Result = {
                 song.ref.moveTo(new File(s"E:/Projects/Mobile/$playlist/${song.filename}"))
-                Ok("song added")
+                Created("song added")
             }
             playlist match {
                 case "good-mood" => moveSong()
@@ -61,5 +66,47 @@ class PlaylistsController @Inject() extends Controller {
         }.getOrElse {
             NotAcceptable("Missing song")
         }
+    }
+
+    def removeSong(playlist: String, songName: String) = Action {
+        (for{
+            path <- playlists.get(playlist)
+            song <- getListOfFiles(path).find(file => file.getName().equals(songName))
+        } yield{
+            song.delete() match {
+                case true => Ok("Song removed")
+                case false => InternalServerError("Error while removing the song")
+            }
+        }).getOrElse {
+            NotFound("The song or the playlist don't exist")
+        }
+
+
+        /*
+        playlists.get(playlist) match {
+            case Some(path) => {
+                getListOfFiles(path).find(file => file.getName().equals(songName)) match{
+                    case Some(song) => {
+                        song.delete() match {
+                            case true => Ok("song removed")
+                            case false => InternalServerError("Error while removing the song")
+                        }
+                    }
+                    case None => NotAcceptable("The song doesn't exist")
+                }
+                //.getOrElse{NotAcceptable("Song doesn't exist")}
+
+
+
+                val songs = for {
+                    file <- getListOfFiles(path)
+                    if file.getName().endsWith(".mp3")
+                } yield _.getName()
+
+
+            }
+            case None => NotAcceptable("The playlist doesn't exist")
+        }
+        */
     }
 }
--- app/controllers/WidgetsController.scala
+++ app/controllers/WidgetsController.scala
@@ -3,6 +3,7 @@ package controllers
 import javax.inject._
 import play.api._
 import play.api.mvc._
+import scala.collection.JavaConversions._
 
 /**
  * This controller creates an `Action` to handle HTTP requests to the
@@ -18,7 +19,17 @@ class WidgetsController @Inject() extends Controller {
    * a path of `/`.
    */
   def getTemp = Action {
-    Ok("It's 25°C in Namur")
+
+
+    val test = play.Play.application.configuration.getConfigList("playlists").foldLeft(""){ (acc, stream) =>
+        acc + s" ${stream.getString("name")} is ${stream.getString("path")}"
+    }
+
+    val playlists: Map[String, String] = (for{
+        configuration <- play.Play.application.configuration.getConfigList("playlists")
+    } yield (configuration.getString("name"), configuration.getString("path"))).toMap
+
+    Ok("It's 25°C in Namur + config : " + play.Play.application.configuration.getString("foo") + " ...  " + test)
   }
 
 }
